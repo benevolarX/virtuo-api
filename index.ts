6fc239fbe06199ea74bb94f15fb628133d711501
benevolar <benevolar.nouveau.project@gmail.com>
2026-01-20 15:37:00 +0100

    first build

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+interface IVirtuoApi {
+  toString(): string 
+}
+
+export class VirtuoApi implements IVirtuoApi {
+
+  constructor(private canvas_id: string) {
+
+  }
+
+  get_canvas_id() {
+    return this.canvas_id
+  }
+
+  try_quit_game() {
+    
+  }
+
+  toString(): string {
+      return "ici virtuo api"
+  }
+}
